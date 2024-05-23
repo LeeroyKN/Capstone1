067a2a4a696c1eff6097f6746fff22eb2e5da732
LeeroyKN <leengoch@gmail.com>
2024-05-23 14:44:57 +0300

    output for Displayname

---
+++ src/output.js
@@ -0,0 +1,14 @@
+function callValue(){
+    var name= document.getElementById("DisplayName");
+    var email = document.getElementById("TeamName");
+    var mobile= document.getElementById("Laptime");
+    var FastestLap= document.getElementById("FastestLap");
+    var Penalties= document.getElementById("Penalties");
+    document.writeln(
+        "Your info: "+"<br"+"your name: "+name +"<br>"
+    );
+    document.writeln(
+        
+    )
+
+}
