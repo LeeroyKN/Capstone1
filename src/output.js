f95ecea47c845b221c108081c5858cf6a03963d1
LeeroyKN <leengoch@gmail.com>
2024-05-23 14:50:27 +0300

    Teamname for output

--- src/output.js
+++ src/output.js
@@ -1,14 +1,16 @@
 function callValue(){
     var name= document.getElementById("DisplayName");
-    var email = document.getElementById("TeamName");
-    var mobile= document.getElementById("Laptime");
+    var Tname = document.getElementById("TeamName");
+    var Ltime= document.getElementById("Laptime");
     var FastestLap= document.getElementById("FastestLap");
     var Penalties= document.getElementById("Penalties");
     document.writeln(
         "Your info: "+"<br"+"your name: "+name +"<br>"
     );
     document.writeln(
-        
+        "Your Teamname: "+Tname
     )
+    
+    
 
 }
