4eddb2a4e5fba428142f40ddb452c35d9f0d85f2
LeeroyKN <leengoch@gmail.com>
2024-05-24 10:00:09 +0300

    updated output for form

--- src/output.js
+++ src/output.js
@@ -1,9 +1,9 @@
 function callValue(){
-    var DisplayName= document.getElementById("DN")
-    var TeamName= document.getElementById("TN")
-    var NoOfLaps= document.getElementById("NL")
-    var FastestLap= document.getElementById("FL")
-    var Penalties= document.getElementById("Pen")
+    var DisplayName= document.getElementById("DN").value;
+    var TeamName= document.getElementById("TN").value;
+    var NoOfLaps= document.getElementById("NL").value;
+    var FastestLap= document.getElementById("FL").value;
+    var Penalties= document.getElementById("Pen").value;
     document.writeln("Your info: "+DisplayName );
     document.write("your Teamname: "+TeamName);
     document.write("your number of laps : "+NoOfLaps);
